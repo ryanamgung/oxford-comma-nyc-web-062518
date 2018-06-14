e7ae5a0348638ac421ae0cdc52a164bba2f6ba87
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-14 20:20:47 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,14 @@
 def oxford_comma(array)
-
+  if array.size == 1
+    string = array.join
+  elsif array.size == 2
+    string = array.join(" and ")
+  else
+    last = array.last
+    array.pop
+    string = array.join(", ")
+    string << ", and #{last}"
+  end
+  
+  
 end
