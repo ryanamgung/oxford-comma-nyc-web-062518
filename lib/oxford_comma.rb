75ce38bb431c73a241008f5f36e21691e2466f7c
Ryan Namgung <heon.namgung@richmond.edu>
2018-06-14 20:20:42 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,13 @@
 def oxford_comma(array)
-  if(array.size > 2)
+  if array.size == 1
+    string = array.join
+  elsif array.size == 2
+    string = array.join(" and ")
+  else
     last = array.last
     array.pop
     string = array.join(", ")
-    string << " and #{last}"
+    string << ", and #{last}"
   end
   
   
